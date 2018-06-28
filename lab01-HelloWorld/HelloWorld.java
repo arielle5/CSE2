2fbfaf7aa38d75083dbff070cbdda53b49e5f28b
arielle5 <arielle5@vt.edu>
2018-06-27 22:39:01 -0400

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////
+//Arielle Carr
+//Demo 0
+//Hello World Java Program
+//
+//  first compile the program
+//    javac HelloWorld.javac
+//  run the program
+//    java HelloWorld//
+
+// define a class
+public class HelloWorld{
+  
+  // add main method
+  public static void main(String[] args){
+    
+    // print the statement with a box around it
+    System.out.println("##############################");
+    System.out.println("##############################");
+    System.out.println("##############################");
+    System.out.println("#                            #");
+    System.out.println("#     Hello World 2018!!     #");
+    System.out.println("#                            #");
+    System.out.println("##############################");
+    System.out.println("test");
+    
+    // test /testtest 
+  }
+  
+}
